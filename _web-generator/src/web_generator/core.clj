4fd2e916c87d929e847fcaede96f372793ad0180
Elio Capella Sánchez <eliocapella@gmail.com>
2016-07-22 14:33:15 +0200

    started generating work-experience html from a data structure

--- _web-generator/src/web_generator/core.clj
+++ _web-generator/src/web_generator/core.clj
@@ -1,7 +1,39 @@
 (ns web-generator.core
+  (:require [web-generator.content.core :as content]
+            [clj-time.core :as time]
+            [clj-time.format :as time-format]
+            [hiccup.core :as hiccup])
   (:gen-class))
+
+(def format-period-date
+  (let [month-year-format (time-format/formatter "MMMM yyyy")]
+    (fn [date] (time-format/unparse month-year-format date))))
+
+(defn period-months
+  [period]
+  (int (/ (time/in-days (time/interval (:start period) (:end period))) 30)))
+
+(defn format-period-length
+  [period]
+  (let [months (period-months period)]
+    (str months " month" (if (not= months 1) "s"))))
+
+(defn format-period
+  [period]
+  (str (format-period-date (:start period))
+       " - "
+       (format-period-date (:end period))
+       ", "
+       (format-period-length period)))
+
+(defn work-experience-html
+  [content]
+  [:section [:div.job-entry
+             [:h2.job-title (:job-title content)]
+             [:p.period (format-period (:period content))]
+             [:p.experience (:experience content)]]])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (hiccup/html (work-experience-html (get content/work-experience 0)))))
