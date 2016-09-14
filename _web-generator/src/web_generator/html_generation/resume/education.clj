8abe1c1adf2723443ede903783804a8b644e94d7
Elio Capella Sánchez <eliocapella@gmail.com>
2016-09-14 17:57:10 +0200

    improved the education section

--- _web-generator/src/web_generator/html_generation/resume/education.clj
+++ _web-generator/src/web_generator/html_generation/resume/education.clj
@@ -3,7 +3,8 @@
   [:div.education-entry
     [:h2.degree (:degree education-entry)]
     [:p.period (:period education-entry)]
-    [:h3.school (:school education-entry)]])
+    [:h3.school (:school education-entry)]
+    [:p.experience (:description education-entry)]])
 
 (defn education-html
   [education]
