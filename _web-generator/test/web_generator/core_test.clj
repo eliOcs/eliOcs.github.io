9218953c9e7fec8c1d9ff7e5aedb37ec6b008dca
Elio Capella <elio.capella@filestage.io>
2018-11-29 16:17:45 +0100

    remove unused test directory

--- _web-generator/test/web_generator/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns web-generator.core-test
-  (:require [clojure.test :refer :all]
-            [web-generator.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
