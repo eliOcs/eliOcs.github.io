9714a1964e9de3f2f96d0653b2cf5de5a1e4838f
Elio Capella <me@eliocapella.com>
2025-12-13 17:25:46 +0100

    continue improving the demo

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+  timeout: 30000,
+  expect: {
+    timeout: 3000,
+  },
   use: {
     baseURL: "http://localhost:3000",
+    actionTimeout: 5000,
   },
   webServer: {
     command: "npm start",
